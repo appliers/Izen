bf356eb10b661a97fc58c529dddbaae76940c7a4
xkwm1 <48575811+xkwm1@users.noreply.github.com>
2020-12-16 21:32:42 -0800

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -1,33 +1,55 @@
-const {
-    Permissions: { FLAGS },
-  } = require("discord.js")
-  
+  const Discord = require('discord.js')
+
   module.exports = {
     name: "clear",
     description: "Clear number of messages in specific channel.",
     args: true,
     usage: "<amount>",
     category: 'moderation',
-    permissions: ['MANAGE_MESSAGES'],
-    permissionError: "You don't have the Manage Messages permission to run this command!",
-    botPermissions: [FLAGS.MANAGE_MESSAGES],
-    userPermissions: [FLAGS.MANAGE_MESSAGES],
   
     run(client, msg, args) {
       const { channel, guild, member } = msg
   
       const amountArg = parseInt(args[0])
+
+      if (!msg.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**You Do Not Have The Required Permissions! - [MANAGE_MESSAGES]**")
   
       if (!Number.isInteger(amountArg)) {
-        return channel.send("You must specify the amount of messages to clear!")
+        const embed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+	      .setTitle("You must specify the amount of message to clear!")
+	      .setTimestamp()
+        return channel.send(embed)
       }
   
       if (amountArg < 2 || amountArg >= 100) {
-        return channel.send(
-          "Amount of messages to clear must be greater than 1 and lower than 100.",
-        )
+        const embed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+	      .setTitle("Amount of message to clear must be greater than 1 and lower than 100.")
+	      .setTimestamp()
+        return channel.send(embed)
+
+
       }
+
   
-      channel.bulkDelete(amountArg)
+      channel.bulkDelete(amountArg).then(() => {
+        const embed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle("")
+        .addFields(
+          { name: 'Messages Deleted:', value: amountArg },
+          { name: 'Cleared By:', value: msg.author.tag },
+          { name: 'Channel:', value: channel },
+        ) 
+        .setTimestamp()
+        return channel.send(embed)
+      })
     },
   }
+
+  /* .catch (e) {
+    console.log(e);
+
+    return msg.channel.send('You can only delete messages that are 14 days old');
+    */
